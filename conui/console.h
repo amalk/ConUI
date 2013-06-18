7517f9c527d5e47da93c503e89f7d3d54f88186c
Elliot Kwan <e_lli_ot@msn.com>
2013-06-17 22:11:38 -0400

    Assignment 1 Complete

--- conui/console.h
+++ conui/console.h
@@ -6,17 +6,24 @@
 
 namespace cui{
 
-
     class Console: public bio::BConsole{
-      static bool _insertMode;
+        static bool _insertMode;
 
     public:
-      void strdsp(const char* str, int row, int col, int len = 0, int curPosition = -1);
-      int  stredit(char *str, int row, int col, int fieldLength, int maxStrLength, 
-                   int* strOffset=(int*)0, int* curPosition=(int*)0,  
-                   bool InTextEditor = false, bool ReadOnly = false, bool& insertMode=_insertMode );
-      static unsigned int _tabsize;
+        void strdsp(const char* str, int row, int col, int len = 0, int curPosition = -1);
+        int  stredit(char* str, int row, int col, int fieldLength, int maxStrLength, 
+                     int* strOffset=(int*)0, int* curPosition=(int*)0,  
+                     bool InTextEditor = false, bool ReadOnly = false, bool& insertMode = _insertMode);
+        static unsigned int _tabsize;
     };
+
+    Console& operator>>(Console& cn, int& ch);
+
+    Console& operator<<(Console& cn, char ch);
+
+    Console& operator<<(Console& cn, const char* str);
+
+	void tabFunction(char* str, int& strLength, int maxStrLength, int& position, int& offset, unsigned int tabsize, const int fieldLength, const bool isInsert);
 
     extern Console console;
 }
