fbb2cde4895c1a38b18061272302260fc02a6222
ArtemL <lisyonok85@yahoo.ca>
2013-07-06 11:30:02 -0400

    Included clabel header

---
+++ Prj/clabel.cpp
@@ -0,0 +1,29 @@
+#include "clabel.h"
+
+namespace cui{
+
+	 CLabel::CLabel(const CLabel& L){
+	 }
+
+     CLabel::CLabel(const char *Str, int Row, int Col, int Len = 0){
+	 }
+
+	 CLabel::CLabel(int Row, int Col, int Len){
+	 }
+
+	 CLabel::~CLabel(){
+	 }
+
+	 void CLabel::draw(int fn=C_NO_FRAME){
+	 }
+
+	 int CLabel::edit(){
+	 }
+
+	 bool CLabel::editable()const{
+	 }
+
+	 void CLabel::set(const void* str){
+	 }
+
+}
---
+++ Prj/clabel.h
@@ -0,0 +1,19 @@
+#ifndef _CLABEL_H_
+#define _CLABEL_H_
+#include "cfield.h"
+
+namespace cui{
+  class CLabel :  public CField{
+     //int _length;
+  public:
+     CLabel(const CLabel& L);
+     CLabel(const char *Str, int Row, int Col, int Len = 0);
+     CLabel(int Row, int Col, int Len);
+     ~CLabel();
+     void draw(int fn=C_NO_FRAME) ;
+     int edit();
+     bool editable()const;
+     void set(const void* str);
+  };
+}
+#endif
