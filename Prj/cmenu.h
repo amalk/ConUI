d2cb0aa751aae841fc70d820ca71e7e1c3769875
Elliot Kwan <e_lli_ot@msn.com>
2013-07-08 15:45:47 -0400

    Threw in all header files

---
+++ Prj/cmenu.h
@@ -0,0 +1,54 @@
+#ifndef __CIO__CMENU_H__
+#define __CIO__CMENU_H__
+#include "cuigh.h"
+#include "cfield.h"
+#include "cmenuitem.h"
+#include "cbutton.h"
+namespace cui{
+  class Cmenu;
+
+  class MNode{
+    CMenuItem* _item;
+    MNode* _next;
+    MNode* _prev;
+    unsigned int _index;
+    MNode(CMenuItem* item,unsigned int index, MNode* prev, MNode* next = ((MNode*)0));
+    ~MNode(void);
+    friend class CMenu;
+  };
+
+  class CMenu : public CField{
+    MNode* _first;
+    MNode* _head;
+    MNode* _tail;
+    MNode* _cur;
+    char _format[3];
+    unsigned int _cnt;
+    int _selectedIndex;
+    bool _dropdown;
+    bool _dropped;
+    bool goNext();
+    bool goPrev();
+    CButton _Title;
+    void drawItems(int fn);
+  public:
+    static const bool Select;
+    CMenu(const char* Title, const char* Format, int Row, int Col, 
+              int Width, int Height,bool dropdown,  
+              const char* Border=C_BORDER_CHARS);
+    CMenu& add(const char* Text, bool selected = false);
+    CMenu& operator<<(const char* Text);
+    CMenu& operator<<(bool select);
+    void draw(int fn = C_FULL_FRAME);
+    int edit();
+    void set(const void* data);
+    int selectedIndex();
+    int selectedIndex(int index);
+    const char* selectedText();
+    bool editable()const;
+    ~CMenu(void);
+  };
+  extern const bool Select;
+}
+
+#endif
---
+++ Prj/ctext.h
@@ -0,0 +1,46 @@
+// Console UI Core Classes
+// text.h
+// 
+// Fardad Soleimanloo, Chris Szalwinski
+// Nov 13 2011
+// Version 0.9
+
+
+
+#pragma once
+#ifndef __CTEXT_H__
+#define __CTEXT_H__
+
+#include "cfield.h"
+#include "text.h"
+
+namespace cui{
+
+
+  class CText:public CField{
+    Text _T;
+    bool _displayOnly;
+    int _curpos;
+    bool* _insertmode;
+    int _offset;
+    int _lcurpos;
+    int _loffset;
+  public:
+    CText(int Row, int Col, int Width, int Height, bool* Insertmode, 
+      bool displayOnly = false, const char* Border=C_BORDER_CHARS);
+    CText(const char* Str, int Row, int Col, int Width, int Height,
+      bool* Insertmode, bool displayOnly = false, 
+      const char* Border=C_BORDER_CHARS);
+    void draw(int fn = C_FULL_FRAME);
+
+    void set(const void *Str);
+    void *data()const;
+
+      int edit();
+    bool  editable()const;
+    bool displayOnly();
+    void displayOnly(bool val);
+  };
+
+}
+#endif
