c291a4f2d0fb574f329a580c91957be311df3fdc
Elliot Kwan <e_lli_ot@msn.com>
2013-08-15 20:56:34 -0400

    CText, CMenu and CDialog comments added. CMenu: deleteNode() updated, safeguard removed

--- Prj/cmenu.cpp
+++ Prj/cmenu.cpp
@@ -32,32 +32,37 @@ CMenu::CMenu(const char* Title, const char* Format, int Row, int Col, int Width,
     _Title.frame(this);
 }
 
+// Adds a new menuitem to the menu through MNodes
 CMenu& CMenu::add(const char* Text, bool selected)
 {
+    // Allocate memory for the MNode and CMenu, with the MNodes index as _cnt and its previous node as _tail
     MNode* newNode = new MNode(new CMenuItem(selected, _format, Text, 1, 1, (width() - 2)), _cnt,
                                _tail);
-    newNode->_item->frame(this);
+    newNode->_item->frame(this);    // Set the new menuitems frame as this
 
+    // Will only occur once at the first addition of a menuitem to the menu
     if(_head == 0)
     {
-        _cur = _first = _head = newNode;
+        _cur = _first = _head = newNode;    // The pointers to the current, first and last nodes are all to t
     }
-    else
+    else    // The previous tail's next is made to point to the new one
     {
         _tail->_next = newNode;
     }
 
-    _tail = newNode;
-    _cnt++;
-    selected && selectedIndex(newNode->_index);
+    _tail = newNode;    // The new tail is the new node
+    _cnt++; // The menuitem count increases
+    selected && selectedIndex(newNode->_index); // Makes the newly added menuitem selected if selected is true
     return *this;
 }
 
+// Calls add
 CMenu& CMenu::operator<<(const char* Text)
 {
     return add(Text);
 }
 
+// Selects the tail
 CMenu& CMenu::operator<<(bool select)
 {
     if(select)
@@ -69,29 +74,36 @@ CMenu& CMenu::operator<<(bool select)
     return *this;
 }
 
+// Draws the menu
 void CMenu::draw(int fn)
 {
+    // Redraw the title if the menu is a dropdown menu
     if(_dropdown)
     {
         _Title.draw();
     }
 
+    // If the menu is dropped or it is not a dropdown menu, draw it
     if(_dropped || !_dropdown)
     {
         CField::draw(fn);
+        // The number of fields to draw changes depending on whether it's bordered or not
         int fieldHeight = (_cnt < height() - 2) ? _cnt : height() - 2;
         int i;
         MNode* temp = _first;
 
+        // Iterate through the fields to be drawn, starting from the _first MNode
         for(i = 0; i < fieldHeight; i++)
         {
-            temp->_item->row((_dropdown ? row() : 1) + i);  //use row if the dropdown is true, otherwise use 1
-            temp->_item->draw();
-            temp = temp->_next;
+            // Set the row of the menuitem in the MNode starting from the row of the menu or 1
+            temp->_item->row((_dropdown ? row() : 1) + i);
+            temp->_item->draw();    // Draw the menuitem
+            temp = temp->_next; // Go to the next MNode
         }
     }
 }
 
+// Edits the menu
 int CMenu::edit()
 {
     int key = 0;
@@ -104,19 +116,20 @@ int CMenu::edit()
     {
         draw();
 
+        // If the menu is a dropdown menu and it hasn't been dropped yet set the edit to the menu's title button
         if(_dropdown && !_dropped)
         {
             key = _Title.edit();
 
             switch(key)
             {
-            case C_BUTTON_HIT:
+            case C_BUTTON_HIT:  // User wants to go into the menu, drop the menu and start editing the menu
                 _dropped = true;
                 doneBrowsing = false;
                 draw();
                 break;
 
-            case LEFT:
+            case LEFT:  // User wants to go past the menu, do not drop or edit the menu
                 key = UP;
                 doneBrowsing = true;
                 break;
@@ -129,7 +142,7 @@ int CMenu::edit()
                 break;
             }
         }
-        else if(!_dropdown)
+        else if(!_dropdown) // If the menu isn't a dropdown menu, start editing
         {
             doneBrowsing = false;
         }
@@ -138,61 +151,72 @@ int CMenu::edit()
 
         while(!doneBrowsing)
         {
-            key = _cur->_item->edit();
+            key = _cur->_item->edit();  // Edit the current MNodes menuitem
 
             switch(key)
             {
-            case ENTER:
+            case ENTER: // If there is a selected index, the key is changed to SPACE and browsing is done
                 (_selectedIndex != -1) && (key = SPACE);
                 doneBrowsing = true;
                 break;
 
-            case SPACE:
+            case SPACE: // Select the current MNode
                 selectedIndex(_cur->_index);
                 doneBrowsing = true;
                 draw();
                 break;
 
-            case DOWN:
+            case DOWN:  // Go to the next MNode, if possible
+                // If it is possible, check to see if the offset of the menu should be changed
                 if(goNext())
                 {
                     if(_cur->_index - _first->_index == height() - 2)
                     {
+                        // _first is moved to the next MNode and the menu is redrawn
                         _first = _first->_next;
                         draw();
                     }
                 }
+                // If it isn't possible, check to see if the user is in a dropdown menu
                 else
                 {
                     if(_dropdown)
                     {
+                        // Change the current and first MNodes to the head
                         _cur = _first = _head;
                         draw();
                     }
                     else
                     {
+                        // Stop editing the menu
                         doneBrowsing = true;
                     }
                 }
 
                 break;
 
-            case UP:
+            case UP:    // Go to the previous MNode, if possible
+                // If it is possible, check to see if the offset of the menu should be changed
                 if(goPrev())
                 {
+                    // Check if the current index is lower than the _first index
                     if(_cur->_index < _first->_index)
                     {
+                        // If so, change the _first index to the previous index and redraw the menu
                         _first = _first->_prev;
                         draw();
                     }
                 }
+                // If it isn't possible, check if the menu is a dropdown menu
                 else
                 {
+                    // If it is a dropdown menu: Set the current index as the tail
                     if(_dropdown)
                     {
                         _cur = _tail;
                         int z = _cnt - height() + 2;
 
+                        // Set _first to the MNode that would be at the top of the menu when the current MNode is at the bottom
                         for(i = 0; i < z; i++)
                         {
                             _first = _first->_next;
@@ -208,7 +232,7 @@ int CMenu::edit()
 
                 break;
 
-            case ESCAPE:
+            case ESCAPE:    // Close the menu if it's a dropdown menu and stop editing
                 _dropped = false;
                 doneEditing = _dropdown ? false : true;
 
@@ -218,13 +242,13 @@ int CMenu::edit()
             }   // switch
         }   // browsing loop
 
-        if(_dropdown)
+        if(_dropdown)   // Hide the menu if it's a dropdown menu
         {
             hide();
         }
     }   // editing loop
 
-    if(_dropdown)
+    if(_dropdown)   // Undrop the menu if it's a dropdown menu and redraw the menu
     {
         _dropped = false;
         draw();
@@ -233,59 +257,70 @@ int CMenu::edit()
     return key;
 }
 
+// Set the selected index to the data entered
 void CMenu::set(const void* data)
 {
     selectedIndex(*(int*)data);
 }
 
+// Return the selected index
 int CMenu::selectedIndex()
 {
     return _selectedIndex;
 }
 
+// Set the selected index as the index entered
 int CMenu::selectedIndex(int index)
 {
     unsigned int i;
-    MNode* temp = _head;
+    MNode* temp = _head;    // Create a temporary pointer to the head of the menu
 
+    // Set all the menuitems in the menu to false
     for(i = 0; i < _cnt && temp; i++, temp = temp->_next)
     {
         temp->_item->selected(false);
     }
 
-    for(temp = _head; temp && temp->_index < index; temp = temp->_next)
-        ;
+    // Iterate through the MNodes until the index matches with the index passed in
+    for(temp = _head; temp && temp->_index < index; temp = temp->_next);
 
+    // If the MNode pointer isn't null, make it selected
     if(temp)
     {
         temp->_item->selected(true);
     }
 
-    return _selectedIndex = index;
+    return _selectedIndex = index; // Set and return the _selected index
 }
 
+// Return the selected menuitem's text
 const char* CMenu::selectedText()
 {
     int selectedIdx = selectedIndex();
 
     if(selectedIdx != -1)
     {
+        // Set the current item as the head
         _cur = _head;
 
-        while(_cur->_index < selectedIdx && goNext())
-            ;
+        // Iterate through the menu to find the selected MNode
+        while(_cur->_index < selectedIdx && goNext());
 
+        // Return the selected MNodes menuitem text
         return _cur->_item->Text();
     }
 
+    // Don't return anything if nothing is selected
     return 0;
 }
 
+// Return true if the menu has 1 or more items
 bool CMenu::editable() const
 {
     return (_cnt > 0);
 }
 
+// Delete all the MNodes
 CMenu::~CMenu()
 {
     while(_head != 0)
@@ -296,6 +331,7 @@ CMenu::~CMenu()
     }
 }
 
+// Returns true if the current node isn't the tail and automatically goes to the next node
 bool CMenu::goNext()
 {
     bool rv = false;
@@ -309,6 +345,7 @@ bool CMenu::goNext()
     return rv;
 }
 
+// Returns true if the current node isn't the head and automatically goes to the previous node
 bool CMenu::goPrev()
 {
     bool rv = false;
@@ -322,64 +359,69 @@ bool CMenu::goPrev()
     return rv;
 }
 
+// Edits the MNode at index i
 void CMenu::editNode(int i, const char* data)
 {
     if(0 <= i && i < _cnt)
     {
         MNode* tNode;
 
-        for(tNode = _head; tNode->_index < i; tNode = tNode->_next)
-            ;
+        // Iterate through the nodes until the index matches i
+        for(tNode = _head; tNode->_index < i; tNode = tNode->_next);
 
+        // Change the menuitems label to data
         tNode->_item->setLabel(data);
     }
 }
 
+// Deletes the MNode at index i
 void CMenu::deleteNode(int i)
 {
-    if(0 <= i && i < _cnt)
+    MNode* toDel;
+    MNode* temp;
+
+    // Iterate through the MNodes until its index matches i
+    for(toDel = _head; toDel && toDel->_index < i; toDel = toDel->_next);
+    // && temp->_index <= _tail->_index
+
+    // Change the index of all the MNodes after the one to be deleted to be one less
+    // Starting from the MNode after the one to be deleted
+    // As long as temp is not null
+    for(temp = toDel->_next; temp; temp = temp->_next)
     {
-        MNode* toDel;
-        MNode* temp;
-
-        for(toDel = _head; toDel && toDel->_index < i; toDel = toDel->_next)
-            ;
-
-        if(_cnt > 1)
-        {
-            for(temp = toDel->_next; temp && temp->_index <= _tail->_index; temp = temp->_next)
-            {
-                temp->_index--;
-            }
-        }
-
-        if(toDel == _head)
-        {
-            _head = _head->_next;
-            _first = _head;
-            _cur = _head;
-        }
-        else
-        {
-            toDel->_prev->_next = toDel->_next;
-        }
-
-        if(toDel == _tail)
-        {
-            _tail = toDel->_prev;
-        }
-        else
-        {
-            toDel->_next->_prev = toDel->_prev;
-        }
-
-        delete toDel;
-        _cnt--;
+        temp->_index--;
     }
+
+    // If the head is to be deleted
+    if(toDel == _head)
+    {
+        // Make the head pointer the one after the current head and make current and first point to it
+        _head = _head->_next;
+        _first = _head;
+        _cur = _head;
+    }
+    // If the tail is to be deleted
+    else if(toDel == _tail)
+    {
+        // Make the tail the one before the current tail and make current point to it
+        _tail = toDel->_prev;
+        _cur = _tail;
+    }
+    else
+    {
+        // Make the MNodes before and after toDel point to eachother
+        toDel->_prev->_next = _cur = toDel->_next;  // Make the current MNode the one after the one to be deleted
+        toDel->_next->_prev = toDel->_prev;
+    }
+
+    delete toDel;
+    _cnt--;
 }
 
+// Move the current MNode to the previous MNode and select it
 void CMenu::navUp(bool allTheWay)
 {
+    // allTheWay is default false and if true will just point the current MNode and the first to the head
     if(allTheWay || _selectedIndex == -1)
     {
         _cur = _first = _head;
@@ -388,14 +430,16 @@ void CMenu::navUp(bool allTheWay)
     {
         _cur = _head;
 
-        //iterate through the MNodes to find the selected menuitem
+        // Iterate through the MNodes to find the selected menuitem
         while(_cur->_index < _selectedIndex)
         {
             _cur = _cur->_next;
         }
 
+        // Check to see if the selected MNode is the head
         if(goPrev())
         {
+            // If not, go up and change the first accordingly
             if(_cur->_index < _first->_index)
             {
                 _first = _first->_prev;
@@ -403,6 +447,7 @@ void CMenu::navUp(bool allTheWay)
         }
         else
         {
+            // If so, set the current MNode to the tail and change first accordingly
             _cur = _tail;
             int z = _cnt - (height() - 2);
 
@@ -417,8 +462,10 @@ void CMenu::navUp(bool allTheWay)
     draw(C_NO_FRAME);
 }
 
+// Move the current MNode to the previous MNode and select it
 void CMenu::navDown(bool allTheWay)
 {
+    // allTheWay is default false and if true will just point the current MNode to the tail and change first accordingly
     if(allTheWay || _selectedIndex == -1)
     {
         _cur = _tail;
@@ -434,14 +481,16 @@ void CMenu::navDown(bool allTheWay)
     {
         _cur = _head;
 
-        //iterate through the MNodes to find the selected menuitem
+        // Iterate through the MNodes to find the selected menuitem
         while(_cur->_index < _selectedIndex)
         {
             _cur = _cur->_next;
         }
 
+        // Check to see if the selected MNode is the tail
         if(goNext())
         {
+            // If not, go down and change the first accordingly
             if(_cur->_index - _first->_index == height() - 2)
             {
                 _first = _first->_next;
@@ -449,6 +498,7 @@ void CMenu::navDown(bool allTheWay)
         }
         else
         {
+            // If so, point current and first to the head
             _cur = _first = _head;
         }
     }
@@ -457,8 +507,10 @@ void CMenu::navDown(bool allTheWay)
     draw(C_NO_FRAME);
 }
 
+// Sets the current MNode to the one with index equal to position
 bool CMenu::navTo(int position)
 {
+    // Returns true if successfully set
     bool temp = false;
 
     if(position <= _cnt && position >= 1)
--- Prj/ctext.cpp
+++ Prj/ctext.cpp
@@ -21,29 +21,37 @@ CText::CText(const char* Str, int Row, int Col, int Width, int Height,
     set(Str);
 }
 
+// Draws the ctext
 void CText::draw(int fn)
 {
     CField::draw(fn);
     int i;
-    int actHeight = height() - 2;
+    int actHeight = height() - 2;   // Always bordered
 
     for(i = 0; i < actHeight; i++)
+    {
+        // Only prints anything if the line is longer than its offset
         if(_T[_loffset + i].strlen() > _offset)
         {
+            // Prints the part of the line after the offset at a length that fits in the box
             console.strdsp(&_T[_loffset + i][_offset], absRow() + i + 1, absCol() + 1, width() - 2, _curpos);
         }
+    }
 }
 
+// Assigns the argument data to the Text held in CText
 void CText::set(const void* Str)
 {
     _T = (char*)Str;
 }
 
+// Returns data from the Text
 void* CText::data()const
 {
     return (void*)_T.exportString();
 }
 
+// Begins editing the CText
 int CText::edit()
 {
     Text local_T      = _T;
@@ -62,20 +70,21 @@ int CText::edit()
     {
         draw();
         currentLine = _lcurpos + _loffset;
-        numLines = _T.textLines();
+        numLines = _T.textLines();  // Keeping track of numLines manually because Text::remove() 
+                                    // doesn't seem to accurately reduce _textlines
         key = console.stredit((_T[currentLine]), absRow() + _lcurpos + 1, absCol() + 1, width() - 2,
                               _T[currentLine].size(), &_offset, &_curpos, true, _displayOnly, *_insertmode);
 
+        // The user will only ever go into this switch statement if he exits the stredit
         switch(key)
         {
-        case UP:
+        case UP: // Go up to the previous line
             goUp(_lcurpos, _loffset);
             break;
 
-        case RIGHT:
-            lineLen = _T[currentLine].strlen();
-
-            if(_curpos + _offset == lineLen && _T[currentLine] != _T[numLines - 1])
+        case RIGHT: // RIGHT only comes out of stredit() if it's pressed at the end of a line
+            // Only go down and change the cursor position/offset if NOT on the last line
+            if(_T[currentLine] != _T[numLines - 1])
             {
                 goDown(_lcurpos, _loffset, height(), numLines);
                 _curpos = _offset = 0;
@@ -83,8 +92,9 @@ int CText::edit()
 
             break;
 
-        case LEFT:
-            if(!_curpos && !_offset && (_lcurpos || _loffset))
+        case LEFT:  // LEFT only comes out of stredit() if it's pressed at the start of a line
+            // Only go up and to the end of the line if the cursor isn't at the first character of the first line
+            if(_lcurpos || _loffset)    // Both are false if cursor is at first character of first line
             {
                 goUp(_lcurpos, _loffset);
                 goRight(_curpos, _offset, _T[_lcurpos + _loffset].strlen(), width());
@@ -92,7 +102,8 @@ int CText::edit()
 
             break;
 
-        case ENTER:
+        case ENTER: // Insert a new line after the current line
+            // Don't insert anything if not editable
             if(!_displayOnly)
             {
                 _T.insertAndBreakAt(currentLine + 1, _curpos + _offset);
@@ -106,19 +117,21 @@ int CText::edit()
             goDown(_lcurpos, _loffset, height(), numLines);
             break;
 
-        case BACKSPACE:
-            if(!_curpos && !_offset && (_lcurpos || _loffset) && !_displayOnly)
+        case BACKSPACE: // BACKSPACE only comes out of stredit() if it's pressed at the start of a line
+            // Only do backspace things if the text is editable and the cursor isn't at the first character of the first line
+            // Backspace things being:
+            if((_lcurpos || _loffset) && !_displayOnly)
             {
                 lineLen = _T[currentLine - 1].strlen();
-                _T[currentLine - 1] += _T[currentLine];
+                _T[currentLine - 1] += _T[currentLine]; // Overwrite the previous lines newline character with everything in the current line
                 _T.remove(currentLine);
-                goUp(_lcurpos, _loffset);
+                goUp(_lcurpos, _loffset);   // Go up and to the end of the previous line
                 goRight(_curpos, _offset, lineLen, width());
             }
 
             break;
 
-        case ESCAPE:
+        case ESCAPE:    // Revert all changes
             _T       = local_T;
             _curpos  = local_curpos;
             _offset  = local_offset;
@@ -143,11 +156,13 @@ bool CText::editable()const
     return true;
 }
 
+// _displayOnly getter
 bool CText::displayOnly()
 {
     return _displayOnly;
 }
 
+// _displayOnly setter
 void CText::displayOnly(bool val)
 {
     _displayOnly = val;
@@ -155,13 +170,13 @@ void CText::displayOnly(bool val)
 
 }
 
-
+// Go up to a previous line changing the offset or cursor position as necessary
 void goUp(int& lcurpos, int& loffset)
 {
     ((lcurpos > 0) && (lcurpos--)) || ((loffset > 0) && (loffset--));
 }
 
-
+// Go down to the next line changing the offset or cursor position as necessary
 void goDown(int& lcurpos, int& loffset, int height, int numLines)
 {
     if(lcurpos < height - 3)
@@ -174,6 +189,7 @@ void goDown(int& lcurpos, int& loffset, int height, int numLines)
     }
 }
 
+// Send the cursor to the end of the line
 void goRight(int& curpos, int& offset, int lineLen, int frameWidth)
 {
     if(lineLen - offset < frameWidth)
